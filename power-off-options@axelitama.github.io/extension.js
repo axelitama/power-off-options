a37e6750af8e9579d2d6b04111186c9dfa75d415
axel <alex.amati@mail.polimi.it>
2025-05-23 00:05:13 +0200

    removed loginManager use and moved settings instantiation to execute it obly one time

--- power-off-options@axelitama.github.io/extension.js
+++ power-off-options@axelitama.github.io/extension.js
@@ -1,6 +1,5 @@
 import GLib from 'gi://GLib';
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as LoginManager from 'resource:///org/gnome/shell/misc/loginManager.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
@@ -30,28 +29,27 @@ export default class PowerOffOptions extends Extension {
     }
 
     enable() {
-        this._initDeferredId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+        this._deferredInitId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
             this._systemMenu = Main.panel.statusArea.quickSettings._system;
-            this._loginManager = LoginManager.getLoginManager();
-            this._settings = this.getSettings();
-            if(!this._systemMenu || !this._loginManager || !this._settings)
+            if(!this._systemMenu)
                 return GLib.SOURCE_CONTINUE;
 
             this._screenOffButton = new ScreenOffButton(this._systemMenu);
             this._hibernationButton = new HibernationButton(this._systemMenu);
 
+            this._settings = this.getSettings();
             this._syncSettings();
             this._settingsConnectionId = this._settings.connect('changed', () => this._syncSettings());
 
-            this._initDeferredId = null;
+            this._deferredInitId = null;
             return GLib.SOURCE_REMOVE;
-        })
+        });
     }
 
     disable() {
-        if(this._initDeferredId) {
-            GLib.Source.remove(this._initDeferredId);
-            this._initDeferredId = null;
+        if(this._deferredInitId) {
+            GLib.Source.remove(this._deferredInitId);
+            this._deferredInitId = null;
         }
 
         if(this._settingsConnectionId) {
@@ -63,7 +61,6 @@ export default class PowerOffOptions extends Extension {
         this._hibernationButton?.destroy();
 
         this._systemMenu = null;
-        this._loginManager = null;
         this._settings = null;
         this._hibernationButton = null;
         this._screenOffButton = null;
